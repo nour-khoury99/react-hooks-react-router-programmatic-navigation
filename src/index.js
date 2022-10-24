aad386dacd4f6e97e710cf5121229889b2cfab0f
nour khoury <khourynour79@gmail.com>
2022-10-24 10:30:07 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router ,useHistory} from "react-router-dom";
 import App from "./components/App";
+
+
+
 
 ReactDOM.render(
   <Router>
